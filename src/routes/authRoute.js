89b336a0799641bf5c4372d0ca5b5ce6e93c334f
this-sunil <swarajya888@gmail.com>
2025-10-04 11:54:17 +0530

    first commit

--- src/routes/authRoute.js
+++ src/routes/authRoute.js
@@ -3,11 +3,11 @@ import { deleteUserController, getAllUserController, getProfileController, login
 import upload from "../middleware/upload.js";
 const router=express.Router();
 
-router.post("/register",upload.none(),registerController);
-router.post("/login",upload.none(),loginController);
+router.post("/register",registerController);
+router.post("/login",loginController);
 router.post("/get-profile",getProfileController);
-router.delete("/deleteUser",upload.none(),deleteUserController);
-router.post("/getAllUser",upload.none(),getAllUserController);
+router.delete("/deleteUser",deleteUserController);
+router.post("/getAllUser",getAllUserController);
 router.post("/update-profile",upload.single("photo"),updateUserController);
 
 export default router;
