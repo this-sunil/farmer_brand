d5028af04835b3495418cd6ea7b4f10ce239fa60
this-sunil <swarajya888@gmail.com>
2025-10-08 17:03:00 +0530

    success

--- src/controller/postController.js
+++ src/controller/postController.js
@@ -110,7 +110,7 @@ export const deletePostController = async (req, res) => {
     console.error(`Error in deletePostController: ${error.message}`);
     return res.status(500).json({
       status: false,
-      msg: `Internal Server Error: ${error.message}`,
+      msg: `Internal Server Error: ${pid}`,
     });
   }
 };
