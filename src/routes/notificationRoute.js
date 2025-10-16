4a04cc742d2f1a782f4bc34bd7ae24daaa8a6aea
this-sunil <swarajya888@gmail.com>
2025-10-16 08:06:23 +0530

    Submit

--- src/routes/notificationRoute.js
+++ src/routes/notificationRoute.js
@@ -1,6 +1,6 @@
 import express from "express";
 import { upload } from "/src/middleware/upload.js";
-import { addNotificationController,deleteNotificationController,fetchNotificationController } from "../controller/notificationController";
+import { addNotificationController,deleteNotificationController,fetchNotificationController } from "../controller/notificationController.js";
 const router=express.Router();
 router.post("/fetchNotification",upload.none(),fetchNotificationController);
 router.post("/addNotification",upload.single("photo"),addNotificationController);
