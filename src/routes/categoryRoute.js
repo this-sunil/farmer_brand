f208950a6cf0b848f7d69de9bec94bf46195df93
this-sunil <swarajya888@gmail.com>
2025-11-02 16:00:40 +0530

    submit

--- src/routes/categoryRoute.js
+++ src/routes/categoryRoute.js
@@ -1,8 +1,9 @@
 import express from "express";
 import upload from "../middleware/upload.js";
-import { addCategoryController, deleteCategoryController, getAllCategory } from "../controller/categoryController.js";
+import { addCategoryController, deleteCategoryController, editCategoryController, getAllCategory } from "../controller/categoryController.js";
 const router=express.Router();
 router.post("/addCategory",upload.single('photo'),addCategoryController);
 router.delete("/deleteCategory",upload.none(),deleteCategoryController);
 router.post("/fetchCategory",upload.none(),getAllCategory);
+router.post("/edit-Category",upload.single('photo'),editCategoryController);
 export default router;
