39ffdc9f40b3ab6f7bfe284fd6280b8d494630af
this-sunil <swarajya888@gmail.com>
2025-11-20 10:38:06 +0530

    save

--- config/firebase.js
+++ config/firebase.js
@@ -1,16 +1,5 @@
 import admin from "firebase-admin";
-
-import { readFileSync } from "fs";
-import { fileURLToPath } from "url";
-import { dirname } from "path";
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
-
-const serviceAccount = JSON.parse(
-  readFileSync(__dirname + "/../config/serviceAccount.json", "utf8")
-);
-
+import serviceAccount from "/../config/serviceAccount.json";
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount)
