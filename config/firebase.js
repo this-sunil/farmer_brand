2b47d30e2c61efde2036ad5f9c4fb0abb5b4356c
this-sunil <swarajya888@gmail.com>
2025-11-20 12:18:21 +0530

    saves

--- config/firebase.js
+++ config/firebase.js
@@ -1,10 +1,10 @@
-import { initializeApp, cert, getApps } from "firebase-admin/app";
+import { initializeApp, getApps, applicationDefault } from "firebase-admin/app";
 import { getMessaging } from "firebase-admin/messaging";
-import serviceAccount from "./serviceAccount.json";
+
 
 if (!getApps().length) {
   initializeApp({
-    credential: cert(serviceAccount),
+    credential: applicationDefault(),
   });
 }
 
