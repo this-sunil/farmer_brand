212ba3d8e662a055e749ee3a8b20d949653ad264
this-sunil <swarajya888@gmail.com>
2025-11-20 12:39:02 +0530

    saves

--- config/firebase.js
+++ config/firebase.js
@@ -1,13 +1,12 @@
-import fs from 'fs';
-import path from 'path';
 import admin from 'firebase-admin';
 
-const serviceAccountPath = path.resolve('./config/serviceAccount.json');
-const serviceAccount = JSON.parse(fs.readFileSync(serviceAccountPath, 'utf8'));
+if (!admin.apps.length) {
+  const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT);
 
-admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
-});
+  admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount),
+  });
+}
 
 export function getAdminMessaging() {
   return admin.messaging();
