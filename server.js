4bbc4e61feaeca886673c0ee7e78ddb699c721b8
this-sunil <swarajya888@gmail.com>
2025-11-22 18:25:54 +0530

    save

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ import bannerRoute from "./src/routes/bannerRoute.js"
 import notificationRoute from "./src/routes/notificationRoute.js";
 import farmerRoute from "./src/routes/farmerRoute.js";
 import productRoute from "./src/routes/productRoute.js";
-import paymentRoute from "./src/routes/paymentRoute.js";
+//import paymentRoute from "./src/routes/paymentRoute.js";
 const app=express();
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
@@ -44,7 +44,7 @@ app.use("/api",notificationRoute);
 app.use("/api",farmerRoute);
 app.use("/api",productRoute);
 
-app.use("/api",paymentRoute);
+//app.use("/api",paymentRoute);
 
 
 
