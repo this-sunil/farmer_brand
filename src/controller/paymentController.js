e0f42592e3213756c0a9114511eec9ec38a1d236
this-sunil <swarajya888@gmail.com>
2025-11-24 10:29:55 +0530

    Submit data

--- src/controller/paymentController.js
+++ src/controller/paymentController.js
@@ -3,75 +3,76 @@ import { generateXVerify } from "../../config/xVerify.js";
 
 // TODO: generate phone pe transaction 
 
-// Source - https://stackoverflow.com/a
-// Posted by deadcoder0904
-// Retrieved 2025-11-24, License - CC BY-SA 4.0
+
 
 
 
 const merchantId = "M23Q1F4U5GOWS";
-const phonePeBaseUrl = "https://api-preprod.phonepe.com/apis/hermes"; // sandbox
+const phonePeBaseUrl = "https://api-preprod.phonepe.com/apis/hermes";
 
-// TODO:create payment
+import crypto from "crypto";
+import axios from "axios";
+
+const SALT_KEY = "YOUR_SANDBOX_SALT_KEY";
+const KEY_INDEX = 1;
+
+const generateXVerify = (payloadBase64, path) => {
+  const toHash = payloadBase64 + path + SALT_KEY;
+  const sha256 = crypto.createHash("sha256").update(toHash).digest("hex");
+  return `${sha256}###${KEY_INDEX}`;
+};
+
 export const createPayment = async (req, res) => {
   try {
     const { amount, userId, mobileNumber } = req.body;
 
-    if (!amount || !userId || !mobileNumber) {
+    if (!amount || !userId || !mobileNumber)
       return res.status(400).json({ success: false, msg: "Missing fields" });
-    }
 
-    const transactionId = `TXN${Date.now().toLocaleString()}`;
+    const transactionId = `TXN_${Date.now()}`;
 
     const payload = {
-      merchantId:merchantId,
+      merchantId,
       merchantTransactionId: transactionId,
       merchantUserId: userId,
       amount: amount * 100,
       redirectMode: "REDIRECT",
       redirectUrl: "https://farmer-brand.vercel.app/",
       callbackUrl: "https://farmer-brand.vercel.app/",
-      mobileNumber:mobileNumber,
+      mobileNumber,
       paymentInstrument: { type: "PAY_PAGE" },
     };
 
     const payloadBase64 = Buffer.from(JSON.stringify(payload)).toString("base64");
 
     const xVerify = generateXVerify(payloadBase64, "/pg/v1/pay");
-    console.log(`Verify=>${xVerify}`);
-    
-    const resp = await axios.post(
+    console.log("X-VERIFY =>", xVerify);
+
+    const response = await axios.post(
       `${phonePeBaseUrl}/pg/v1/pay`,
       { request: payloadBase64 },
       {
         headers: {
           "Content-Type": "application/json",
           "X-VERIFY": xVerify,
-          "X-MERCHANT-ID": merchantId
-        }
+          "X-MERCHANT-ID": merchantId,
+        },
       }
     );
-    if(resp.status==200){
+
     return res.status(200).json({
       success: true,
       payloadBase64,
       xVerify,
-      phonepe: resp.data,
+      phonepe: response.data,
     });
-  }
-  else{
-    return res.status(404).json({
-      status:false,
-      msg:"Something Went Wrong"
-    });
-  }
-
 
   } catch (err) {
     console.error("PHONEPE ERROR:", err?.response?.data || err);
     res.status(500).json({ success: false, error: err.message });
   }
 };
+
 
 
 
