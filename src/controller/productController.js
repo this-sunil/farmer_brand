3d54f27d8d1d2cf5e2b7d90cf1ca6495c9e4bf33
this-sunil <swarajya888@gmail.com>
2025-11-25 12:20:05 +0530

    Save

--- src/controller/productController.js
+++ src/controller/productController.js
@@ -107,8 +107,8 @@ export const addQtyController = async (req, res) => {
       });
     }
     console.log(`Qty =>${req.body.qty === 0}`);
-    
-   if(Number(qty) === 0){
+    const quantity=Number(qty);
+   if(quantity === 0){
      console.log(`Qty =>${qty}`);
      const query=`DELETE FROM users_product WHERE uid=$1 AND pid=$2`;
      const {rows}=await pool.query(query,[uid,pid]);
