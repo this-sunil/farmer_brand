355a95cd00285b6fe3d904f7d64dfdc6602515d8
this-sunil <swarajya888@gmail.com>
2025-11-25 11:01:14 +0530

    Save table

--- src/controller/productController.js
+++ src/controller/productController.js
@@ -96,7 +96,7 @@ export const addQtyController = async (req, res) => {
     }
 
     if(qty===0){
-     const query=`DELETE products FROM pid=$1 AND uid=$2`;
+     const query=`DELETE users_product FROM pid=$1 AND uid=$2`;
      const {rows}=await pool.query(query);
      if(rows.length>0){
       return res.status(200).json({
