d0b143229e659ffaa4141d3cd2ed57f651b08373
this-sunil <swarajya888@gmail.com>
2025-11-26 09:20:19 +0530

    Submit

--- src/middleware/upload.js
+++ src/middleware/upload.js
@@ -1,5 +1,5 @@
 import multer from "multer";
-import { CloudinaryStorage } from "@fluidjs/multer-cloudinary";
+import multerCloudinary from "multer-cloudinary";
 import { v2 as cloudinary } from "cloudinary";
 
 cloudinary.config({
@@ -8,7 +8,7 @@ cloudinary.config({
   api_secret: process.env.CLOUDINARY_API_SECRET,
 });
 
-const storage = new CloudinaryStorage({
+const storage = new multerCloudinary({
   cloudinary,
   params: {
     folder: "uploads",                  
