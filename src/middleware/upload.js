f028ab455bb41ce8a3f13e670da3e4806839481c
this-sunil <swarajya888@gmail.com>
2025-11-26 12:45:25 +0530

    Save

--- src/middleware/upload.js
+++ src/middleware/upload.js
@@ -8,7 +8,7 @@ cloudinary.config({
   api_secret: process.env.CLOUDINARY_API_SECRET,
 });
 
-const storage = multerCloudinary({
+const storage =new multerCloudinary({
   cloudinary: {
     cloudinary: cloudinary,
   },
