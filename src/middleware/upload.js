8019227059fafefa2581da29aff781704ee48a50
this-sunil <swarajya888@gmail.com>
2025-12-10 11:24:19 +0530

    Submit

--- src/middleware/upload.js
+++ src/middleware/upload.js
@@ -2,7 +2,6 @@ import multer from "multer";
 import cloudinary from "cloudinary";
 import CloudinaryStorage from "multer-storage-cloudinary";
 
-
 cloudinary.config({
   cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
   api_key: process.env.CLOUDINARY_API_KEY,
