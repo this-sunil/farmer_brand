56f08074c72273035298f427f0bba0498bf069e9
this-sunil <swarajya888@gmail.com>
2025-12-12 11:15:11 +0530

    save

--- src/public/js/custom.js
+++ src/public/js/custom.js
@@ -4,36 +4,3 @@ $(document).ready(function(){
         $(".row").slideToggle();
     });
 });
-// Smooth Scroll
-document.querySelectorAll('a[href^="#"]').forEach(anchor => {
-    anchor.addEventListener("click", function (e) {
-        e.preventDefault();
-        document.querySelector(this.getAttribute("href")).scrollIntoView({
-            behavior: "smooth"
-        });
-    });
-});
-// Scroll Spy (Highlight navbar menu)
-const sections = document.querySelectorAll("section");
-const navLinks = document.querySelectorAll(".nav-item .nav-link a");
-
-function activateMenu() {
-    let currentSection = "";
-
-    sections.forEach(section => {
-        const sectionTop = section.offsetTop - 150;
-        if (pageYOffset >= sectionTop) {
-            currentSection = section.getAttribute("id");
-        }
-    });
-
-    navLinks.forEach(link => {
-        link.classList.remove("active");
-        if (link.getAttribute("href") === `#${currentSection}`) {
-            link.classList.add("active");
-        }
-    });
-}
-
-window.addEventListener("scroll", activateMenu);
-
