71708960345bf6c23569e5fb83d447dd438b1405
this-sunil <swarajya888@gmail.com>
2025-12-12 11:10:18 +0530

    save

--- src/public/js/custom.js
+++ src/public/js/custom.js
@@ -4,3 +4,19 @@ $(document).ready(function(){
         $(".row").slideToggle();
     });
 });
+// Reveal on scroll
+function revealOnScroll() {
+    let elements = document.querySelectorAll('.reveal');
+
+    elements.forEach((el) => {
+        let position = el.getBoundingClientRect().top;
+        let screenHeight = window.innerHeight;
+
+        if (position < screenHeight - 120) {
+            el.classList.add('active');
+        }
+    });
+}
+
+window.addEventListener('scroll', revealOnScroll);
+window.onload = revealOnScroll;
