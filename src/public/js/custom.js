1b6e065ffc0ccc80fdbf7be61a4bddc32deb9381
this-sunil <swarajya888@gmail.com>
2025-12-23 13:20:30 +0530

    save

--- src/public/js/custom.js
+++ src/public/js/custom.js
@@ -3,5 +3,24 @@ $(document).ready(function(){
         $(".menu span").toggleClass('active');
         $(".row").slideToggle();
     });
+
+ 
+  $('.play-btn').click(function () {
+    const video = $(this).siblings('video')[0];
+
+    if (video.paused) {
+      video.play();
+      $(this).hide();
+    } else {
+      video.pause();
+      $(this).show();
+    }
+  });
+
+  $('video').on('pause ended', function () {
+    $(this).siblings('.play-btn').show();
+  });
+
+
 });
 
