c46443fd1ae4a92271b6c8dc082fffa88c8bfd09
this-sunil <swarajya888@gmail.com>
2025-12-23 13:52:33 +0530

    save

--- src/public/js/custom.js
+++ src/public/js/custom.js
@@ -5,7 +5,21 @@ $(document).ready(function(){
     });
 
  
+  $('.video-card').each(function () {
+    const video = $(this).find('video')[0];
+    const button = $(this).find('.play-button');
+    const icon = button.find('i');
 
+    button.on('click', function () {
+      if (video.paused) {
+        video.play();
+        icon.removeClass('fa-play').addClass('fa-pause');
+      } else {
+        video.pause();
+        icon.removeClass('fa-pause').addClass('fa-play');
+      }
+    });
+  });
 
 
 });
